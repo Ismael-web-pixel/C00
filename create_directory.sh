769d99adf70d56533bbe40982e0993ed62107f5c
Godisouleymane <ssabiougodi@gmail.com>
2025-05-07 00:09:28 +0100

    push

--- create_directory.sh
+++ create_directory.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+
 
 # Crée un répertoire nommé test_directory sans erreur si déjà présent
-mkdir -p test_directory
+mkdir  test_directory
 
-# Message de confirmation
-echo "Le répertoire 'test_directory' a été créé (ou existait déjà)."
+
